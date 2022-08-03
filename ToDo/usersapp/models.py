3e8bc870fdacb760843ec0c9ceffe4b9e31972fd
Anastasia <Ambrella915@gmail.com>
2022-08-03 14:36:47 +0300

    [+] Создана модель пользователя с уникальным полем email

--- ToDo/usersapp/models.py
+++ ToDo/usersapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=160, unique=True)
