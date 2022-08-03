b84cc37c8ba0e343763ee7cbf2a03879651163cc
Anastasia <Ambrella915@gmail.com>
2022-08-03 14:58:36 +0300

    [+] Создала файл для сериализации всех полей (в дальнейшем нужно изменить)

---
+++ ToDo/usersapp/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
+
+from .models import Users
+
+
+class UsersModelSerializer(ModelSerializer):
+    class Meta:
+        model = Users
+        fields = '__all__'
