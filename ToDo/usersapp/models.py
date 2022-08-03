205465ca1043cd00dce146f22e9da5489fb84009
Anastasia <Ambrella915@gmail.com>
2022-08-03 16:25:16 +0300

    [~] Изменила модель users. Добавила в настройках модель по умолчанию

--- ToDo/usersapp/models.py
+++ ToDo/usersapp/models.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Users(models.Model):
+class Users(AbstractUser):
     username = models.CharField(max_length=64)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
