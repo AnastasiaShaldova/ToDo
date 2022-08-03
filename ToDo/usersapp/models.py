6a19cb474dbd6bec303272c67fd216abf83b224f
Anastasia <Ambrella915@gmail.com>
2022-08-04 00:06:02 +0300

    [~] Изменена модель Users. [+] Добавлен абстрактюзер и доп.поля.

--- ToDo/usersapp/models.py
+++ ToDo/usersapp/models.py
@@ -3,7 +3,13 @@ from django.db import models
 
 
 class Users(AbstractUser):
-    username = models.CharField(max_length=64)
+    username = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
+    password = models.CharField(max_length=24, default='')
     email = models.EmailField(max_length=160, unique=True)
+    is_active = models.BooleanField(default=True)
+
+
+
+
