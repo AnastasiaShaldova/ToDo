4ad5072338f0d06012fd94e6308a879ef1a904e8
Anastasia <Ambrella915@gmail.com>
2022-08-04 12:49:59 +0300

    [~] Изменена модель пользователей

--- ToDo/usersapp/models.py
+++ ToDo/usersapp/models.py
@@ -3,12 +3,9 @@ from django.db import models
 
 
 class Users(AbstractUser):
-    username = models.CharField(max_length=50, unique=True)
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
-    password = models.CharField(max_length=24, default='')
     email = models.EmailField(max_length=160, unique=True)
-    is_active = models.BooleanField(default=True)
+
+
 
 
 
