004bb3f8075126797974aa6dda236af66eedd123
Anastasia <Ambrella915@gmail.com>
2022-08-08 12:33:31 +0300

    [+] Создала временный список пользователей

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import UsersList from "./components/users";
 
 class App extends React.Component {
   constructor(props) {
@@ -9,10 +10,32 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    const usersapp = [
+      {
+        'username': 'Ivan',
+        'first_name': 'Иванов',
+        'last_name': 'Иван',
+        'email': 'ivan@ivan.ru',
+      },
+      {
+        'username': 'Frodo',
+        'first_name': 'Федор',
+        'last_name': 'Федоров',
+        'email': 'frodo@frodo.ru',
+      },
+    ]
+    this.setState(
+        {
+          'usersapp': usersapp
+        }
+    )
+  }
+
   render() {
     return (
         <div>
-          Main App
+          <UsersList usersapp={this.state.usersapp}/>
         </div>
     )
   }
