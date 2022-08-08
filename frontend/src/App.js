a501519a35dc06d4356c4b29ccae8150504dbe79
Anastasia <Ambrella915@gmail.com>
2022-08-08 13:38:43 +0300

    [+] Добавлены настройки из бэкэнд

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import UsersList from "./components/user";
+import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
@@ -11,25 +12,28 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    const users = [
-      {
-        'username': 'Ivan',
-        'first_name': 'Иванов',
-        'last_name': 'Иван',
-        'email': 'ivan@ivan.ru',
-      },
-      {
-        'username': 'Frodo',
-        'first_name': 'Федор',
-        'last_name': 'Федоров',
-        'email': 'frodo@frodo.ru',
-      },
-    ]
-    this.setState(
+    axios.get('http://127.0.0.1:8000/api/users/').then(response => {
+      this.setState(
         {
-          'usersapp': users
+          'usersapp': response.data
         }
     )
+    }).catch(error => console.log(error))
+    // const users = [
+    //   {
+    //     'username': 'Ivan',
+    //     'first_name': 'Иванов',
+    //     'last_name': 'Иван',
+    //     'email': 'ivan@ivan.ru',
+    //   },
+    //   {
+    //     'username': 'Frodo',
+    //     'first_name': 'Федор',
+    //     'last_name': 'Федоров',
+    //     'email': 'frodo@frodo.ru',
+    //   },
+    // ]
+
   }
 
   render() {
