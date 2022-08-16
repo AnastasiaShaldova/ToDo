7fff169bdc0d5130355394aec6c9c818728a8f01
Anastasia <Ambrella915@gmail.com>
2022-08-16 08:47:33 +0300

    [~] Исправила модели

--- ToDo/taskapp/models.py
+++ ToDo/taskapp/models.py
@@ -5,13 +5,13 @@ from usersapp.models import Users
 
 class Project(models.Model):
     title = models.CharField(max_length=60)
-    link_to_repository = models.URLField
+    link_to_repository = models.URLField(help_text='Project url', null=True, blank=True)
     users = models.OneToOneField(Users, on_delete=models.CASCADE)
     pass
 
 
 class Tasks(models.Model):
-    projects = models.OneToOneField(Project, on_delete=models.CASCADE)
+    projects = models.ManyToManyField(Project)
     text = models.TextField(blank=True, null=True)
     date_create = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True)
