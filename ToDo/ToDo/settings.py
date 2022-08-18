52a54c261672b14d93694a31fbcce16777772c23
Anastasia <Ambrella915@gmail.com>
2022-08-18 18:59:40 +0300

    [+] Добавлена настройка пагинации на 100 стр

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -93,6 +93,9 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
         # Any other parsers
     ),
+    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 WSGI_APPLICATION = 'ToDo.wsgi.application'
