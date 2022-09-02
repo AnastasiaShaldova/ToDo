ce24282d6c6fa4809dfa9dd206b65541c82d6f61
Anastasia <Ambrella915@gmail.com>
2022-09-02 20:51:41 +0300

    [+] Создала компонент, получила токен, сохранила токен

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import AppHeader from "./components/header";
 import UsersList from "./components/user";
 import TasksList from "./components/tasks";
-import TasksUser from "./components/tasksUser";
+// import TasksUser from "./components/tasksUser";
 import AppFooter from "./components/footer";
 import NotFound404 from "./components/NotFound404";
+import LoginForm from "./Auth";
 import axios from "axios";
+import Cookies from "universal-cookie";
 import {BrowserRouter, Route, Routes, Navigate, NavLink} from 'react-router-dom';
-
 
 
 class App extends React.Component {
@@ -17,11 +18,30 @@ class App extends React.Component {
         this.state = {
             'users': [],
             'tasks': [],
+            'token': '',
         }
     }
+    is_auth(){
+        return !!this.state.token
+    }
+    set_token(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token}, () => this.load_data())
+        console.log(this.state.token)
+    }
 
-    componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/users').then(response => {
+    get_token(username, password) {
+        const data = {username: username, password: password}
+        axios.post('http://127.0.0.1:8000/api-token-auth/', data)
+            .then(response => {
+                this.set_token(response.data['token'])
+            }).catch(error => alert('Неверный логин или пароль'))
+    }
+
+    load_data() {
+        const headers = this.get_headers()
+        axios.get('http://127.0.0.1:8000/api/users', {headers}).then(response => {
             console.log(response.data)
             this.setState(
                 {
@@ -30,7 +50,7 @@ class App extends React.Component {
             )
         }).catch(error => console.log(error))
 
-        axios.get('http://127.0.0.1:8000/api/tasks').then(response => {
+        axios.get('http://127.0.0.1:8000/api/tasks', {headers}).then(response => {
             console.log(response.data)
             this.setState(
                 {
@@ -40,18 +60,35 @@ class App extends React.Component {
         }).catch(error => console.log(error))
     }
 
+    get_headers() {
+        let headers = {
+            'Content-Type':'application/json'
+        }
+        if (this.is_auth()) {
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        return headers
+    }
+
+    componentDidMount() {
+        this.load_data()
+    }
+
     render() {
         return (
             <BrowserRouter>
-                    <NavLink to='/'>Главная</NavLink>
-                    <NavLink to='/users'>Пользователи</NavLink>
-                    <NavLink to='/tasks'>Записки</NavLink>
+                <NavLink to='/'>Главная</NavLink>
+                <NavLink to='/users'>Пользователи</NavLink>
+                <NavLink to='/tasks'>Записки</NavLink>
+                <NavLink to='/login'>Логин</NavLink>
                 <Routes>
                     <Route path="/" element={<AppHeader header={this.state.header}/>}></Route>
                     <Route path="/" element={<AppFooter footer={this.state.footer}/>}></Route>
                     <Route path="/users" element={<UsersList users={this.state.users}/>}></Route>
                     <Route path="/tasks" element={<TasksList tasks={this.state.tasks}/>}></Route>
-                    <Route path='/users/:user'><TasksUser users={this.state.users}/></Route>
+                    <Route path="/login" element={
+                        <LoginForm get_token={(username, password) => this.get_token(username, password)}/>}></Route>
+                    {/*<Route path='/users/:user'><TasksUser users={this.state.users}/></Route>*/}
                     <Route path="/task" element={<Navigate to='/tasks'/>}></Route>
                     <Route path="/*" element={<NotFound404/>}></Route>
                 </Routes>
--- frontend/src/components/tasksUser.js
+++ frontend/src/components/tasksUser.js
@@ -1,14 +1,13 @@
 import React from "react";
-import {useParams} from "react-router";
+// import {useParams} from "react-router";
 
 
 const TasksItem = ({task}) => {
     return (
         <tr>
-            <td>{task.url}</td>
+            <td>{task.id}</td>
             <td>{task.text}</td>
             <td>{task.dateCreate}</td>
-            <td>{task.dateUpdate}</td>
             <td>{task.projects}</td>
             <td>{task.user}</td>
         </tr>
@@ -16,21 +15,20 @@ const TasksItem = ({task}) => {
 }
 
 const TasksUser = ({tasks}) => {
-    let {id} = useParams()
-    console.log(id)
-    let filter_tasks = tasks.filters((task => task.task.username.includes(parseInt(id))))
+    // let {id} = useParams()
+    // console.log(id)
+    // let filter_tasks = tasks.filters((task => task.task.username.includes(parseInt(id))))
     return (
-            <table className="table">
-                <tr>
-                    <th>url</th>
-                    <th>text</th>
-                    <th>date_create</th>
-                    <th>date_update</th>
-                    <th>projects</th>
-                    <th>user</th>
-                </tr>
-                {filter_tasks.map((task) => <TasksItem task={task}/>)}
-            </table>
+        <table className="table">
+            <tr>
+                <th>id</th>
+                <th>text</th>
+                <th>date_create</th>
+                <th>projects</th>
+                <th>user</th>
+            </tr>
+            {tasks.map((task) => <TasksItem task={task}/>)}
+        </table>
 
 
     )
