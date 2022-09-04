a7b1a9be33ff9763516701ff0fef47fd15742401
Anastasia Shaldova <83241853+AnastasiaShaldova@users.noreply.github.com>
2022-09-04 21:37:39 +0300

    Update views.py

--- ToDo/usersapp/views.py
+++ ToDo/usersapp/views.py
@@ -6,17 +6,17 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Users
 from .serializers import UsersModelSerializer
 
-#
-# class SuperUserOnly(BasePermission):
-#     def has_permission(self, request, view):
-#         return request.user.is_staff
+
+ class SuperUserOnly(BasePermission):
+     def has_permission(self, request, view):
+         return request.user.is_staff
 
 
-# class UsersModelViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
-#                         mixins.UpdateModelMixin, viewsets.GenericViewSet):
-#     queryset = Users.objects.all()
-#     serializer_class = UsersModelSerializer
-#     permission_classes = [SuperUserOnly]
+ class UsersModelViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
+                         mixins.UpdateModelMixin, viewsets.GenericViewSet):
+     queryset = Users.objects.all()
+     serializer_class = UsersModelSerializer
+     permission_classes = [SuperUserOnly]
 
 
 class UsersModelViewSet(ModelViewSet):
