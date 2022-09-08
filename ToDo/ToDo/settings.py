42ea14ef183d8df4cef97c36930925739467e1c7
Anastasia <Ambrella915@gmail.com>
2022-09-08 18:01:50 +0300

    [+] Сбилдила и подключила бэк и фронт

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'ToDo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'frontend/build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -183,6 +183,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (BASE_DIR/'frontend/build/static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- ToDo/ToDo/urls.py
+++ ToDo/ToDo/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from graphene_django.views import GraphQLView
@@ -57,6 +58,8 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc')),
 
     path('graphql/', GraphQLView.as_view(graphiql=True)),
+
+    path('', TemplateView.as_view(template_name='index.html'))
 
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
